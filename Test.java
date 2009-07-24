1093aac40ec01a216637c9eae06e9ab27d118cdf
Anthony Robinson <anthor@pdx.edu>
2009-07-23 19:18:29 -0700

    added menu place holder, changed background colors to white

--- Test.java
+++ Test.java
@@ -9,39 +9,59 @@ import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Color;
+
 
 
 public class Test extends JFrame {
 
     public Test() {
+        Color backgroundColor = new Color(255,255,255);
+
         setSize(400,400);
-        setTitle("Test Window");
-   
+        setTitle("Tic Tac Toe v2.0");
+        setResizable(false); // May change depending on requirements.
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         Toolkit toolkit = getToolkit();
         Dimension size = toolkit.getScreenSize();
         setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
 
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         add(mainPanel);
         mainPanel.add(Box.createVerticalGlue());
+        mainPanel.setBackground(backgroundColor);
+
         JPanel topPanel = new JPanel();
         topPanel.setAlignmentX(.5f);
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        topPanel.setBackground(backgroundColor);
+        topPanel.add(new JLabel("Blah"));
 
         mainPanel.add(topPanel);
-        topPanel.add(new JLabel("Blah"));
+        
+
 
         JPanel bottomPanel = new JPanel();
         bottomPanel.setAlignmentX(.5f);
         bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        bottomPanel.setBackground(backgroundColor);
         mainPanel.add(bottomPanel);
 
         JPanel bottomLeftPanel = new JPanel();
         bottomLeftPanel.setAlignmentX(.5f);
         bottomLeftPanel.setLayout(new BoxLayout(bottomLeftPanel, BoxLayout.X_AXIS));
         bottomLeftPanel.add(new JLabel("Blah"));
+        bottomLeftPanel.setBackground(backgroundColor);
+        
         bottomPanel.add(bottomLeftPanel);
         bottomPanel.add(Box.createHorizontalGlue());
 
@@ -51,6 +71,7 @@ public class Test extends JFrame {
         boardPanel.setMaximumSize(new Dimension(300,250));
         boardPanel.setAlignmentX(0f);
         boardPanel.setAlignmentY(.5f);
+        boardPanel.setBackground(backgroundColor);
         ImageIcon boardSquare = new ImageIcon("square.png");
         
         // ulSquare1.setBorderPainted(true);
