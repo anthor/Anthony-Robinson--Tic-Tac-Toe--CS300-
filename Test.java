a68e2b09439701b6e50feeed32c15eeca8e624d4
Anthony Robinson <anthor@pdx.edu>
2009-07-23 17:41:03 -0700

    added buttons in a 3x3 grid

--- Test.java
+++ Test.java
@@ -4,9 +4,11 @@
  * Change in master...
  * Experiment version
  */
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+
 
 public class Test extends JFrame {
 
@@ -17,6 +19,27 @@ public class Test extends JFrame {
         Toolkit toolkit = getToolkit();
         Dimension size = toolkit.getScreenSize();
         setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
+
+        JPanel boardPanel = new JPanel();
+        boardPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        boardPanel.setLayout(new GridLayout(3,3,5,5));
+
+        ImageIcon boardSquare = new ImageIcon("square.png");
+        // JButton ulSquare1 = new JButton(boardSquare);
+        // ulSquare.setAlignmentX(0.5f);
+        // ulSquare.setAlignmentY(0.5f);
+        // ulSquare.setSize(80,80);
+        // ulSquare1.setBorderPainted(true);
+        // ulSquare1.setContentAreaFilled(false);
+        // ulSquare1.setFocusPainted(false);
+
+        int i;
+        for(i=0;i<9;++i)
+        {
+        boardPanel.add(new JButton(boardSquare));
+        }
+
+        add(boardPanel);
     }
 
     public static void main(String[] args)
