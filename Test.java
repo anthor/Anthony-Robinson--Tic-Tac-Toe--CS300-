218be2c226d6433a70542eaf6bb3ff686d7ddd31
Anthony Robinson <anthor@pdx.edu>
2009-07-28 16:58:05 -0700

    about to do some heavy changes to the layout and images

--- Test.java
+++ Test.java
@@ -15,100 +15,102 @@ import java.awt.Color;
 
 public class Test extends JFrame {
 
-    public Test() {
-        Color backgroundColor = new Color(255,255,255);
-
-        setSize(400,400);
-        setTitle("Tic Tac Toe v2.0");
-        setResizable(false); // May change depending on requirements.
-        
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Toolkit toolkit = getToolkit();
-        Dimension size = toolkit.getScreenSize();
-        setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
-
-
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File");
-
-        menuBar.add(menu);
-        setJMenuBar(menuBar);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        add(mainPanel);
-        mainPanel.add(Box.createVerticalGlue());
-        mainPanel.setBackground(backgroundColor);
-
-        JPanel topPanel = new JPanel();
-        topPanel.setAlignmentX(.5f);
-        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
-        topPanel.setBackground(backgroundColor);
-        topPanel.add(new JLabel("Blah"));
-
-        mainPanel.add(topPanel);
-        
-
-
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setAlignmentX(.5f);
-        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
-        bottomPanel.setBackground(backgroundColor);
-        mainPanel.add(bottomPanel);
-
-        JPanel bottomLeftPanel = new JPanel();
-        bottomLeftPanel.setAlignmentX(.5f);
-        bottomLeftPanel.setLayout(new BoxLayout(bottomLeftPanel, BoxLayout.X_AXIS));
-        bottomLeftPanel.add(new JLabel("Blah"));
-        bottomLeftPanel.setBackground(backgroundColor);
-        
-        bottomPanel.add(bottomLeftPanel);
-        bottomPanel.add(Box.createHorizontalGlue());
-
-        JPanel boardPanel = new JPanel();
-        boardPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        boardPanel.setLayout(new GridLayout(3,3,0,0));
-        boardPanel.setMaximumSize(new Dimension(300,250));
-        boardPanel.setAlignmentX(0f);
-        boardPanel.setAlignmentY(.5f);
-        boardPanel.setBackground(Color.black);
-
-        ImageIcon boardSquare = new ImageIcon("square.png");
-        ImageIcon boardSquareOver = new ImageIcon("squareover.png");
-        // ulSquare1.setBorderPainted(true);
-        // ulSquare1.setContentAreaFilled(false);
-        // ulSquare1.setFocusPainted(false);
-
-        int i;
-        for(i=0;i<9;++i)
+    public Test()
         {
-        JButton temp = new JButton(boardSquare);
-        temp.setRolloverIcon(boardSquareOver);
-        temp.setMargin(new Insets(0,0,0,0));
-        temp.setBorderPainted(false);
-        temp.setIconTextGap(0);
-        temp.setContentAreaFilled(false);
-        temp.setFocusPainted(false);
-        boardPanel.add(temp);
+            Color backgroundColor = new Color(255,255,255);
+
+            setSize(400,400);
+            setTitle("Tic Tac Toe v2.0");
+            setResizable(true); // May change depending on requirements.
+
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            Toolkit toolkit = getToolkit();
+            Dimension size = toolkit.getScreenSize();
+            setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
+
+
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+
+            menuBar.add(menu);
+            setJMenuBar(menuBar);
+
+            JPanel mainPanel = new JPanel(); // Main panel will contain all the other panels.
+            mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+            add(mainPanel);
+            mainPanel.add(Box.createVerticalGlue());
+            mainPanel.setBackground(backgroundColor);
+
+            JPanel topPanel = new JPanel();
+            topPanel.setAlignmentX(.5f);
+            topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+            topPanel.setBackground(backgroundColor);
+
+            topPanel.add(new JLabel("Blah"));
+
+            mainPanel.add(topPanel);
+
+
+
+            JPanel middlePanel = new JPanel();
+            middlePanel.setAlignmentX(.5f);
+            middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.X_AXIS));
+            middlePanel.setBackground(backgroundColor);
+            mainPanel.add(middlePanel);
+
+            JPanel middleLeftPanel = new JPanel();
+            middleLeftPanel.setAlignmentX(.5f);
+            middleLeftPanel.setLayout(new BoxLayout(middleLeftPanel, BoxLayout.X_AXIS));
+            middleLeftPanel.add(new JLabel("Blah"));
+            middleLeftPanel.setBackground(backgroundColor);
+
+            middlePanel.add(middleLeftPanel);
+            middlePanel.add(Box.createHorizontalGlue());
+
+            JPanel boardPanel = new JPanel();
+            boardPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+            boardPanel.setLayout(new GridLayout(3,3,0,0));
+            boardPanel.setMaximumSize(new Dimension(300,250));
+            boardPanel.setAlignmentX(0f);
+            boardPanel.setAlignmentY(.5f);
+            boardPanel.setBackground(Color.black);
+
+            ImageIcon boardSquare = new ImageIcon("square.png");
+            ImageIcon boardSquareOver = new ImageIcon("squareover.png");
+            // ulSquare1.setBorderPainted(true);
+            // ulSquare1.setContentAreaFilled(false);
+            // ulSquare1.setFocusPainted(false);
+
+            int i;
+            for(i=0;i<9;++i)
+                {
+                JButton temp = new JButton(boardSquare);
+                temp.setRolloverIcon(boardSquareOver);
+                temp.setMargin(new Insets(0,0,0,0));
+                temp.setBorderPainted(false);
+                temp.setIconTextGap(0);
+                temp.setContentAreaFilled(false);
+                temp.setFocusPainted(false);
+                boardPanel.add(temp);
+                }
+
+            middlePanel.add(boardPanel);
+            middlePanel.add(Box.createHorizontalGlue());
+
+
+            JPanel middleRightPanel = new JPanel();
+            middleRightPanel.setAlignmentX(.5f);
+            middleRightPanel.setLayout(new BoxLayout(middleRightPanel, BoxLayout.X_AXIS));
+            middleRightPanel.add(new JLabel("Blah"));
+            middlePanel.add(middleRightPanel);
+
+
         }
 
-        bottomPanel.add(boardPanel);
-        bottomPanel.add(Box.createHorizontalGlue());
-
-
-        JPanel bottomRightPanel = new JPanel();
-        bottomRightPanel.setAlignmentX(.5f);
-        bottomRightPanel.setLayout(new BoxLayout(bottomRightPanel, BoxLayout.X_AXIS));
-        bottomRightPanel.add(new JLabel("Blah"));
-        bottomPanel.add(bottomRightPanel);
-   
-
-    }
-
     public static void main(String[] args)
-    {
-    Test test = new Test();
-    test.setVisible(true);
-    }
+        {
+        Test test = new Test();
+        test.setVisible(true);
+        }
 }
 
